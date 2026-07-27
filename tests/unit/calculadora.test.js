d8ca8262b66c46a41cf67000bbc7d6f3b5a89e0f
Ryan <ryanhougaske@gmail.com>
2026-07-27 17:07:33 -0400

    add: 'api/v1/status'

---
+++ tests/unit/calculadora.test.js
@@ -0,0 +1,21 @@
+const calculadora = require("../../models/Calculadora");
+
+test("Deve somar 2 + 2 e retornar 4", () => {
+    const resultado = calculadora.somar(2, 2);
+    expect(resultado).toBe(4);
+});
+
+test("Deve somar 50 + 10 e retornar 60", () => {
+    const resultado = calculadora.somar(50, 10);
+    expect(resultado).toBe(60);
+});
+
+test("Deve somar 'banana' + 10 e retornar 60", () => {
+    const resultado = calculadora.somar("banana", 10);
+    expect(resultado).toBe("Erro");
+});
+
+test("Deve somar 10 +'banana' e retornar 60", () => {
+    const resultado = calculadora.somar(10, "banana");
+    expect(resultado).toBe("Erro");
+});
